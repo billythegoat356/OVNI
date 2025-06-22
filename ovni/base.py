73bc15f02635bb361761a1f1e9621e0cf5695faf
Fedora <billythegoat356@gmail.com>
2025-06-22 16:39:31 +0200

    reformated

--- ovni/base.py
+++ ovni/base.py
@@ -7,14 +7,16 @@ import PyNvVideoCodec as nvc
 import cupy as cp
 import pycuda.autoinit # Required
 
-from kernels import *
-
 
 # Constants
 GPU_ID = 0
 CODEC = 'h264' # Encoding codec
 BITRATE = '5M'
 PRESET = 'P3' # 1-7, determines quality, but impacts speed
+
+
+# Set Warning log level (not sure if this works)
+nvc.logger.setLevel(nvc.logging.WARNING)
 
 
 # Initialize cuda device and context
@@ -250,55 +252,3 @@ def mux(h264_stream: Iterable[bytes], output_path: str) -> None:
 
 
 
-def test():
-
-    import time
-
-    start = time.time()
-
-    frames = demux_and_decode(
-        input_path="videos/video2.mp4",
-        frame_count=None
-    )
-
-
-    t = 0
-
-    def process_frames(frames):
-        nonlocal t
-        for frame in frames:
-
-            yield frame
-            t += 1
-
-
-    frames = pipe_nv12_to_rgb(frames, 1920, 1080)
-
-    frames = process_frames(frames)
-
-    frames = pipe_rgb_to_nv12(frames, 1920, 1080)
-
-    h264_stream = encode(
-        frames=frames,
-        width=1920,
-        height=1080,
-        fps=25
-    )
-
-    mux(
-        h264_stream=h264_stream,
-        output_path="videos/out.mp4"
-    )
-
-
-    print(f"Took {round(time.time() - start, 2)}s")
-    print(f"Frames: {t}")
-
-
-    d = t/25
-    s = round(d/(time.time() - start), 2)
-    print(f"Speed: x{s}")
-
-
-if __name__ == '__main__':
-    test()
