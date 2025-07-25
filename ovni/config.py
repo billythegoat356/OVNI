da6ea47c1156f773737da3a94928fb1fad5d995c
Fedora <billythegoat356@gmail.com>
2025-07-26 00:58:50 +0200

    added __init__ file for ovni package

--- ovni/config.py
+++ ovni/config.py
@@ -2,12 +2,12 @@ import PyNvVideoCodec as nvc
 
 
 
-# Constants (adjust code if you need customzz)
+# Constants (defaults for parameters)
 GPU_ID = 0
 CODEC = 'h264' # Encoding codec
 BITRATE = '5M'
 PRESET = 'P3' # 1-7, determines quality, but impacts speed
 
 
-# Set Warning log level (not sure if this works)
+# Set Warning log level (not 100% sure if this works)
 nvc.logger.setLevel(nvc.logging.WARNING)
